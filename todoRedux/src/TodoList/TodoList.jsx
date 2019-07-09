5ebab47a29d55c9bb0b87bec13267d196b5a57b8
tuvshin <tuvshin.otgo@gmail.com>
2019-07-09 22:20:49 +0800

    splitted and added todolistview micro component

--- todoRedux/src/TodoList/TodoList.jsx
+++ todoRedux/src/TodoList/TodoList.jsx
@@ -1,10 +1,7 @@
 import React, { PureComponent } from 'react';
-import {connect} from 'react-redux';
-
+import { connect } from 'react-redux';
 import { addTodo, removeTodo, isDoneToggle } from '../store/actions/todo';
-import './TodoList.css';
-import Input from './components/Input/Input';
-import Listing from './components/Listing/Listing';
+import TodoListView from './components/TodoListView/TodoListView';
 
 class TodoList extends PureComponent {
 
@@ -42,27 +39,22 @@ class TodoList extends PureComponent {
     }
 
     render() {
-        return (
-            <div className="TodoList">
-                <Input InputHandler={this.inputHandler}
-                    inputValue={this.state.inputValue}
-                    addHandler={this.props.addTodo}
-                    keyPressHandler={this.keyPress}
-                    clearInputValue={this.clearInputValue}
-                    isValid={this.state.isValid} />
-                <Listing listings={this.props.Listing}
-                    isDoneHandler={this.props.isDoneToggle}
-                    removeListHandler={this.props.removeTodo} />
-            </div>
-        )
+        return <TodoListView inputHandler={this.inputHandler}
+            inputValue={this.state.inputValue}
+            addHandler={this.props.addTodo}
+            keyPressHandler={this.keyPress}
+            clearInputValue={this.clearInputValue}
+            isValid={this.state.isValid}
+            listings={this.props.Listing}
+            isDoneHandler={this.props.isDoneToggle}
+            removeListHandler={this.props.removeTodo} />
     }
 }
 
 const mapStateToProps = (state) => {
     return {
-        Listing : state.todo.Listing
+        Listing: state.todo.Listing
     };
 };
-
 
 export default connect(mapStateToProps, { addTodo, removeTodo, isDoneToggle })(TodoList);
---
+++ todoRedux/src/TodoList/components/TodoListView/TodoListView.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './TodoListView.css';
+import Input from '../Input/Input';
+import Listing from '../Listing/Listing';
+
+const todoListView = props => (
+    <div className="TodoList">
+        <Input inputHandler={props.inputHandler}
+            inputValue={props.inputValue}
+            addHandler={props.addHandler}
+            keyPressHandler={props.keyPressHandler}
+            clearInputValue={props.clearInputValue}
+            isValid={props.isValid} />
+        <Listing listings={props.listings}
+            isDoneHandler={props.isDoneHandler}
+            removeListHandler={props.removeListHandler} />
+    </div>
+)
+
+export default todoListView;
