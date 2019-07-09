b69301d057a754056161908cbdf0aa4c0deab8e1
tuvshin <tuvshin.otgo@gmail.com>
2019-07-09 20:23:20 +0800

    splitted Listing to ListingItem micro component

--- todoContainer/src/TodoList/components/Listing/Listing.jsx
+++ todoContainer/src/TodoList/components/Listing/Listing.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
+import ListingItem from './ListingItem/ListingItem';
 import './Listing.css';
 
 const listing = (props) => {
     const listing = props.listings.map((list, idx) => {
-        return <li key={idx}
-                    className={list.isDone ? 'Done' : ''} 
-                    onClick={() => props.isDoneHandler(idx)}>
-                    {list.isDone ? <span className="check">Done</span> : ''} 
-                    {list.value} 
-                    <span className="close" onClick={(e) => props.removeListHandler(e, idx)}>x</span>
-                </li>
+        return <ListingItem idx={idx}
+            list={list}
+            isDoneHandler={props.isDoneHandler}
+            removeListHandler={props.removeListHandler} />
     })
 
     const toDo = (
@@ -18,7 +16,7 @@ const listing = (props) => {
         </ul>
     )
 
-    return props.listings.length !== 0 ? toDo : <h1 style={{paddingBottom:'25px', color:'#f44336'}}>Start Your Schedule Now!</h1> 
+    return props.listings.length !== 0 ? toDo : <h1 style={{ paddingBottom: '25px', color: '#f44336' }}>Start Your Schedule Now!</h1>
 }
 
 export default listing;
